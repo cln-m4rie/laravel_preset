85cfaa2ee05a77d4b1d6bcc2bedbffd12f9731a4
cln-m4rie <s.kikkawa0613@gmail.com>
2020-02-29 14:02:24 +0900

    change model path

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-02-28 19:49:49.
+    * Generated on 2020-02-29 05:01:31.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
